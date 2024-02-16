bdfc7628cd57a4a724f36c045563093c182e0a11
AlperenKalyoncu <145429690+AlperenKalyoncu@users.noreply.github.com>
2024-02-16 21:31:13 +0300

    Add files via upload

--- characterContrroller.cs
+++ characterContrroller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,58 @@ public class characterContrroller : MonoBehaviour
 {
 
     Rigidbody2D rigid;
-    public float force = 0;
+    public float speed = 0;
     Vector2 yon;
-    // Start is called before the first frame update
+
+    // yeniden doðarken ayarlayýn publicleri
+    public bool ableToJump = true;
+    public float jumpForce = 0f; // zýplama gücü 3 baþýna karakter boyu
+
+    public bool ableToFly = false;
+    bool onFloor;
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        yon = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        yon.Normalize();
-        rigid.velocity = yon * force;
+        yon = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        move();
     }
 
+    private void move()
+    {
+        if (ableToFly)
+        {
+            rigid.velocity = yon * speed;
+            return;
+        }
+
+        yon = new Vector2(yon.x, 0);
+        yon.Normalize();
+        rigid.velocity = new Vector2(yon.x * speed, rigid.velocity.y);
+
+        if (onFloor && ableToJump && Input.GetButtonDown("Jump")) 
+        {
+            onFloor = false;
+            rigid.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("floor")) 
+        {
+            onFloor = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("floor"))
+        {
+            onFloor = false;
+        }
+    }
 }
